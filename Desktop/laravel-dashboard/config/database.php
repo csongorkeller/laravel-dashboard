38761bf8365727baee070b1479fe5b49fd76a458
Csongor Keller <44320707+csongorkeller@users.noreply.github.com>
2019-02-04 22:28:12 +0100

    Update database.php

--- Desktop/laravel-dashboard/config/database.php
+++ Desktop/laravel-dashboard/config/database.php
@@ -41,11 +41,11 @@ return [
                 //Call db connections
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '185.43.207.2'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'webmetr_userbase'),
-            'username' => env('DB_USERNAME', 'webmetr_userbase'),
-            'password' => env('DB_PASSWORD', ',pW[$ckYC^bA'),
+            'host' => env('DB_HOST', '******'),
+            'port' => env('DB_PORT', '******'),
+            'database' => env('DB_DATABASE', '******'),
+            'username' => env('DB_USERNAME', '******'),
+            'password' => env('DB_PASSWORD', ',******'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -56,10 +56,10 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'host' => env('DB_HOST', '******'),
+            'port' => env('DB_PORT', '******'),
+            'database' => env('DB_DATABASE', '******'),
+            'username' => env('DB_USERNAME', '******'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
@@ -69,10 +69,10 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'host' => env('DB_HOST', '******'),
+            'port' => env('DB_PORT', '******'),
+            'database' => env('DB_DATABASE', '******'),
+            'username' => env('DB_USERNAME', '******'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
@@ -109,9 +109,9 @@ return [
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'host' => env('REDIS_HOST', '******'),
+            'password' => env('REDIS_PASSWORD', ******),
+            'port' => env('REDIS_PORT', ******),
             'database' => 0,
         ],
 
